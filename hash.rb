e66861dc5f7b355f82da1b6e46ff16e9eb10b0e4
bellDev-code <sumaoo20@naver.com>
2022-05-26 00:05:34 +0900

    ruby hash example

--- hash.rb
+++ hash.rb
@@ -2,5 +2,11 @@ params = {}
 
 params[:user] = { name: "Michael Hartl", email: "sumaoo20@naver.com"}
 
-puts params
-puts params[:user][:email]
+# puts params
+# puts params[:user][:email]
+
+flash = { success: "It worked!", danger: "It failed." }
+
+flash.each do |key, value|
+    puts "Key #{key.inspect} has value #{value.inspect}"
+end
