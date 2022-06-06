87e286001f5faaf044399905cca0c93e14d65170
bellDev-code <sumaoo20@naver.com>
2022-06-06 23:19:45 +0900

    token

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  # 추가 #
   # 입력받은 문자열의 해시값을 리턴
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -16,4 +15,20 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   # 추가 #
+
+  # 랜덤한 토근을 생성하여 리턴
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 영속적인 세션을 위해 유저를 데이터베이스에 기록한다.
+  def remember
+    self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 입력받은 토큰이 Digest와 일치하면 true를 리턴한다.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
