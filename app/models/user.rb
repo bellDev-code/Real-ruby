8cbb6a227ee97ee15e89c57fd857c019c8186f56
tomspacecloud <tom@spacecloud.kr>
2022-06-06 21:58:30 +0900

    User test error code 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
@@ -8,10 +8,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 추가 #
   # 입력받은 문자열의 해시값을 리턴
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::password.create(string, cost: cost)
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
+  # 추가 #
 end
